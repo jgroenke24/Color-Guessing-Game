8e317838266aacb4b509037d4013898266fcee85
Jorge <jorge.groenke@gmail.com>
2018-08-08 18:48:44 +0000

    Add colors to boxes in javascript

--- colorgame.js
+++ colorgame.js
@@ -1,1 +1,15 @@
-alert("connected");
+let colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+];
+
+let squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+
+for(let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+}
