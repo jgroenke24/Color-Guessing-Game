bfd76382eaefd3ad2aa593832aaa5ee2f9dca0ec
Jorge <jorge.groenke@gmail.com>
2018-08-09 00:56:12 +0000

    Add function to pick random color from array

--- colorgame.js
+++ colorgame.js
@@ -8,7 +8,7 @@ let colors = [
 ];
 
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 
@@ -38,4 +38,9 @@ function changeColors(color) {
         // Change each color to match given color
         squares[i].style.backgroundColor = color;
     }
+}
+
+function pickColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
