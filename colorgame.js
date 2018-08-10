2115a50087a98a686a797f301b5f5ca6fa367a9a
Jorge <jorge.groenke@gmail.com>
2018-08-10 00:49:22 +0000

    Refactor code for difficulty mode buttons and reset

--- colorgame.js
+++ colorgame.js
@@ -6,52 +6,41 @@ let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
 let resetButton = document.getElementById("reset");
-let easyBtn = document.getElementById("easyBtn");
-let hardBtn = document.getElementById("hardBtn");
+let modeButtons = document.querySelectorAll(".mode");
 
-easyBtn.addEventListener("click", function() {
-    hardBtn.classList.remove("selected");
-    easyBtn.classList.add("selected");
-    numSquares = 3;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(let i = 0; i < squares.length; i++) {
-        if (colors[i]) {
-            squares[i].style.background = colors[i];
-        } else {
-            squares[i].style.display = "none";
-        }
-    }
-});
+for(let i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function() {
+        modeButtons[0].classList.remove("selected");
+        modeButtons[1].classList.remove("selected");
+        this.classList.add("selected");
+        this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
+        reset();
+    });
+}
 
-hardBtn.addEventListener("click", function() {
-    hardBtn.classList.add("selected");
-    easyBtn.classList.remove("selected");
-    numSquares = 6;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(let i = 0; i < squares.length; i++) {
-        squares[i].style.background = colors[i];
-        squares[i].style.display = "block";
-    }
-});
-
-resetButton.addEventListener("click", function() {
+function reset() {
     // Generate all new colors
     colors = generateRandomColors(numSquares);
     // Pick new random color from array
     pickedColor = pickColor();
     // Change colorDisplay to match pickedColor
     colorDisplay.textContent = pickedColor;
-    this.textContent = "New Colors";
+    resetButton.textContent = "New Colors";
     messageDisplay.textContent = "";
     // Change colors of squares
     for(let i = 0; i < squares.length; i++) {
-        squares[i].style.background = colors[i];
+        if (colors[i]) {
+            squares[i].style.display = "block";
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
     }
     h1.style.background = "steelblue";
+}
+
+resetButton.addEventListener("click", function() {
+    reset();
 });
 
 colorDisplay.textContent = pickedColor;
